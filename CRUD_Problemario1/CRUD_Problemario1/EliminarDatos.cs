5361a345062353e5048e4db5caf554433682471c
Ruben Catalan Gutierrez <byrubenss@gmail.com>
2019-11-14 14:43:23 -0600

    CRUD Principal y agregar

---
+++ CRUD_Problemario1/CRUD_Problemario1/EliminarDatos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Problemario1
+{
+    internal class EliminarDatos
+    {
+        internal void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CRUD_Problemario1/CRUD_Problemario1/Principal.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CRUD_Problemario1
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        List<Biblioteca.Biblioteca_De_Musica> bibliotecas;
+        public Principal()
+        {
+            InitializeComponent();
+            bibliotecas = new List<Biblioteca.Biblioteca_De_Musica>();
+            LeerBaseDatos();
+        }
+        void LeerBaseDatos()
+        {
+            using (SQLite.SQLiteConnection conn =
+                new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Biblioteca.Biblioteca_De_Musica>();
+                bibliotecas = (conn.Table<Biblioteca.Biblioteca_De_Musica>().ToList()).
+                    OrderBy(c => c.Cancion).ToList();
+            }
+            if (bibliotecas != null)
+            {
+                lvBiblioteca.ItemsSource = bibliotecas;
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            CRUD_Problemario1.MainWindow form = new CRUD_Problemario1.MainWindow();
+            form.ShowDialog();
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            EliminarDatos form = new CRUD_Problemario1.EliminarDatos();
+            form.ShowDialog();
+        }
+    }
+}
